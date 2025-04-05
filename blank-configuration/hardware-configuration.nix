5a1536c64d0b43b07e0f748aeacccbfaac47cd7e
admiral-shlork <vvhtvr@protonmail.ch>
2025-04-05 16:24:21 +0100

    updated flake.lock

--- blank-configuration/hardware-configuration.nix
+++ blank-configuration/hardware-configuration.nix
@@ -28,4 +28,8 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
+
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
 }
