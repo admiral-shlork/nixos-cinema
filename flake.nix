174abb9e44f6675ddd250f1d82d18f645d8a582b
admiral-shlork <vvhtvr@protonmail.ch>
2026-06-19 20:42:40 +0100

    changing nixpkgs.url

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "blah Flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
