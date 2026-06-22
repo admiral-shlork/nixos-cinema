e11820ef030f954b0b2fb9e274691f96b6e739a2
admiral-shlork <vvhtvr@protonmail.ch>
2026-06-22 16:27:50 +0100

    small refactor

--- blank-configuration/networking-configuration.nix
+++ blank-configuration/networking-configuration.nix
@@ -1,9 +1,14 @@
-{ config, pkgs, lib, modulesPath, ... }:
+{ config, lib, modulesPath, pkgs, ... }:
 
 {
   networking = {
     hostName = "cinema";
     useDHCP = lib.mkDefault true;
     networkmanager.enable = true;
+    firewall = {
+      allowedTCPPorts = [ 9000 ];
+      allowedUDPPorts = [ ];
+      trustedInterfaces = [  ];
+    };
   };
 }
